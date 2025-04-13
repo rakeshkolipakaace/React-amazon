6fd7504cddef47b7a724daf7adc4bf9bea0fb702
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-13 11:12:06 +0530

    card product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-const Product =(items)=> {
+// const Product = ({ items }) => {
+//   return (
+//     <>
+//       <div className="container">
+//         <div className="row d-flex justify-content-center">
+//           {items.map((product) => (
+//             <div
+//               key={product.id}
+//               className="col-lg-4 col-md my-3 d-flex justify-content-center"
+//             >
+//               <div className="card product-card" style={{ width: '18rem' }}>
+//                 <Link to={`/product/${product.id}`} className="image-container">
+//                   <img
+//                     src={product.imgsrc}
+//                     className="card-img-top"
+//                     alt={product.title}
+//                   />
+//                 </Link>
+//                 <div className="card-body">
+//                   <h5 className="card-title">{product.title}</h5>
+//                   <p className="card-text">{product.description}</p>
+//                   <p className="card-text fw-bold">₹{product.price}</p>
+//                   <Link to={`/product/${product.id}`} className="btn btn-primary">
+//                     View Details
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Product;
+
+
+
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Product = ({ items }) => {
   return (
-    <div>Product</div>
-  )
+    <>
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          {items.map((product) => (
+            <div
+              key={product.id}
+              className="col-lg-4 col-md my-3 d-flex justify-content-center"
+            >
+              <div className="card product-card" style={{ width: "18rem" }}>
+                <Link to={`/product/${product.id}`} className="image-container">
+                  <img
+                    src={product.imgSrc}
+                    className="card-img-top"
+                    alt={product.title}
+                  />
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Product
+export default Product;
